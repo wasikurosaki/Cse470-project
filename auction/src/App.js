383bcb7caf60a250ac9f160264bd12da4f0caccf
Lasaadu <al.amin1@g.bracu.ac.bd>
2024-02-15 17:08:45 +0600

    saad4

---
+++ auction/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<>
+<div className='bg-white text-blue-600 items-center flex flex-col '>
+
+  hello world
+
+</div>
+</>
+  );
+}
+
+export default App;
