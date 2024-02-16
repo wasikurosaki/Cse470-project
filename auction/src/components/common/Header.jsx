b3b99bf2ea85f0ba29a437ff6e6ca3226d30d6de
Lasaadu <al.amin1@g.bracu.ac.bd>
2024-02-17 01:14:12 +0600

    responsive

--- auction/src/components/common/Header.jsx
+++ auction/src/components/common/Header.jsx
@@ -21,30 +21,32 @@ const Header = () => {
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex flex-row gap-2 items-center">
+          <div className="flex items-center">
             <div className="flex-shrink-0">
-              <img className="h-36 w-auto" src={Img} alt="Your Company" />
+              <img className="h-8 w-auto" src={Img} alt="Your Company" />
             </div>
             {/* Links */}
-            <Link
-              to="/"
-              className="text-white rounded-md px-3 py-2 text-sm font-medium"
-              aria-current="page"
-            >
-              Home
-            </Link>
-            <Link
-              to="/admin"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-            >
-              Admin
-            </Link>
-            <Link
-              to="/customer"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-            >
-              Customer
-            </Link>
+            <div className="flex space-x-4">
+              <Link
+                to="/"
+                className="text-white rounded-md px-3 py-2 text-sm font-medium"
+                aria-current="page"
+              >
+                Home
+              </Link>
+              <Link
+                to="/admin"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              >
+                Admin
+              </Link>
+              <Link
+                to="/customer"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              >
+                Customer
+              </Link>
+            </div>
           </div>
           <div className="flex items-center space-x-4">
             {!userLoggedIn ? (
