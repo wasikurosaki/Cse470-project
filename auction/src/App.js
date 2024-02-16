b7bcc87e5c2fb6b72d747b28840aa6986696b7ed
wasikurosaki <washikur.rahman@g.bracu.ac.bd>
2024-02-16 21:44:07 +0600

    updates

--- auction/src/App.js
+++ auction/src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/common/Header";
+import LoginFrom from "./components/auth/LoginFrom";
+import RegForm from "./components/auth/RegForm";
+import AdminDashboard from "./components/admin/AdminDashboard";
+import CustomerDashboard from "./components/customer/CustomerDashboard";
+import ChangePass from "./components/auth/ChangePass";
+import Landingpage from "./components/LandingPage/Landingpage";
 
 function App() {
   return (
-<>
-<div className='bg-white text-blue-600 items-center flex flex-col '>
+    <Router>
+      <Header />
 
-  hello world
-
-</div>
-</>
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/customer" element={<CustomerDashboard />} />
+        <Route path="/signup" element={<RegForm />} />
+        <Route path="/login" element={<LoginFrom />} />
+        <Route path="/changepass" element={<ChangePass />} />
+      </Routes>
+    </Router>
   );
 }
 
